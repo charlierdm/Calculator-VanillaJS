d68abeea41fd958ed24ad6c368bf0af214ce0d63
Charlie <charlie.davismiller@gmail.com>
2021-09-10 20:14:04 +0100

    data added to HTML and JS variables created to select elements

---
+++ main.js
@@ -0,0 +1,8 @@
+const number = document.querySelectorAll('[data-number]')
+const operation = document.querySelectorAll('[data-operation]')
+const equals = document.querySelector('[data-equals]')
+const allClear = document.querySelector('[data-all-clear]')
+const deleteButton = document.querySelector('[data-delete]')
+const prevOperation = document.querySelector('[data-prev-operation]')
+const currentOperation = document.querySelector('[data-current-operation]')
+
