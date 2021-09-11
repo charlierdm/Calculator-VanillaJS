d6c1fcd2a05d8ae01f6343ec29920e2d20f1c6a6
Charlie <charlie.davismiller@gmail.com>
2021-09-11 22:54:11 +0100

    allClearButton now clears the calculators display

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 class Calculator {
   constructor(prevOperandText, currentOperandText) {
-    this.prevOperandText = prevOperandText
-    this.currentOperandText = currentOperandText
-    this.clear()
+    this.prevOperandText = prevOperandText;
+    this.currentOperandText = currentOperandText;
+    this.clear();
   }
 
   clear() {
-    this.prevOperand= "";
+    this.prevOperand = "";
     this.currentOperand = "";
-    this.operation = undefined
+    this.operation = undefined;
   }
 
   delete() {
@@ -16,7 +16,7 @@ class Calculator {
   }
 
   appendNumber(number) {
-    this.currentOperand = number
+    this.currentOperand = number;
   }
 
   chooseOperation(operation) {
@@ -24,28 +24,32 @@ class Calculator {
   }
 
   evaluate() {
-
+    
   }
 
   updateScreen() {
-    this.currentOperandText.innerText = this.currentOperand
+    this.currentOperandText.innerText = this.currentOperand;
   }
 }
 
-const numberButtons = document.querySelectorAll('[data-number]')
-const operationButton = document.querySelectorAll('[data-operation]')
-const equalsButton = document.querySelector('[data-equals]')
-const allClearButton = document.querySelector('[data-all-clear]')
-const deleteButton = document.querySelector('[data-delete]')
-const prevOperationText = document.querySelector('[data-prev-operation]')
-const currentOperationText = document.querySelector('[data-current-operation]')
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButton = document.querySelectorAll("[data-operation]");
+const equalsButton = document.querySelector("[data-equals]");
+const allClearButton = document.querySelector("[data-all-clear]");
+const deleteButton = document.querySelector("[data-delete]");
+const prevOperationText = document.querySelector("[data-prev-operation]");
+const currentOperationText = document.querySelector("[data-current-operation]");
 
-const calculator = new Calculator(prevOperationText, currentOperationText)
+const calculator = new Calculator(prevOperationText, currentOperationText);
 
-numberButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    calculator.appendNumber(button.innerText)
-    calculator.updateScreen()
-  })
-})
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.appendNumber(button.innerText);
+    calculator.updateScreen();
+  });
+});
 
+allClearButton.addEventListener("click", () => {
+  calculator.clear();
+  calculator.updateScreen();
+});
